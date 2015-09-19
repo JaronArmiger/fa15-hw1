aa65d7431c79a1be66dea59674590f91037167dc
Samboy Kirk <jaron.armiger@gmail.com>
2015-09-18 19:00:17 -0700

    my hw1

--- foobar.rb
+++ foobar.rb
@@ -1,3 +1,13 @@
 class Foobar
-  # Q4 CODE HERE
+  def initialize(arr)
+  	@arr = arr
+  end
+  def self.baz
+  	@arr.each { |i| i.to_i }
+  	@arr.each { |i| i + 2 }
+  	@arr.select! { |i| i.even? }
+  	@arr.uniq
+  	@arr.reject { |i| i > 10 }
+  	@arr.inject(0) { |sum, i| sum + i }
+  end
 end
